99998907f61c49a1edf4a33d2272ec4a5d6ba1f6
Noobgam <shessmaster12@gmail.com>
2018-09-01 23:39:26 +0300

    Adding stuff

--- act.cpp
+++ act.cpp
@@ -1,11 +1,25 @@
+#include <thread>
+
 namespace olymplib {
 
-template <class InputIt, class Callable>
-void CallStuff(InputIt begin, InputIt end, Callable callee, int threads = 1)
+template <class InputIter, class Callable>
+void CallStuff(InputIter begin, InputIter end, Callable callee)
 {
     while (begin != end) {
         callee(*begin++);
     }
+}
+
+template <class RandomAccessIter, class Callable>
+void CallStuff(RandomAccessIter begin, RandomAccessIter end, Callable callee, int threads) {
+    std::thread* roflmao = new std::thread[threads];
+    for (int i = 0; i < threads; ++i) {
+        roflmao[i] = std::thread(CallStuff<RandomAccessIter, Callable>, (begin * (threads - i) + end * i) / threads, (begin * (threads - i - 1) + end * (i + 1)) / threads, callee);        
+    }
+    for (int i = 0; i < threads; ++i) {
+        roflmao[i].join();
+    }
+    delete[] roflmao;
 }
 
 } // namespace olymplib
--- act_ut.cpp
+++ act_ut.cpp
@@ -4,15 +4,30 @@
 using namespace olymplib;
 
 TEST(SIMPLE_ACT, SQUARE) {
-    constexpr int TESTLEN = 10;
-    int* a = new int[TESTLEN];
+    constexpr int TESTLEN = 1e8;
+    long long* a = new long long[TESTLEN];
     for (int i = 0; i < TESTLEN; ++i) {
         a[i] = i;
     }
-    CallStuff(a, a + TESTLEN, [](int& a) { a *= a; });
+    CallStuff(a, a + TESTLEN, [](long long& a) { a *= a; });
     
     for (int i = 0; i < TESTLEN; ++i) {
-        EXPECT_EQ(a[i], i * i);
+        EXPECT_EQ(a[i], 1LL * i * i);
     }
     delete[] a;
 }
+
+TEST(PARALLEL_ACT, SQUARE) {
+    constexpr int TESTLEN = 1e8;
+    long long* a = new long long[TESTLEN];
+    for (int i = 0; i < TESTLEN; ++i) {
+        a[i] = i;
+    }
+    CallStuff(a, a + TESTLEN, [](long long& a) { a *= a; }, 4);
+    
+    for (int i = 0; i < TESTLEN; ++i) {
+        EXPECT_EQ(a[i], 1LL * i * i);
+    }
+    delete[] a;
+}
+
