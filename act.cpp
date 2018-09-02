2f4d7bb50375285bf6cf8e2cf321b4bd4020d677
Noobgam <shessmaster12@gmail.com>
2018-09-02 03:09:33 +0300

    OL-2 initial commit

--- act.cpp
+++ act.cpp
@@ -2,30 +2,7 @@
 
 namespace olymplib {
 
-template <class InputIter, class Callable>
-void CallStuff(InputIter begin, InputIter end, Callable callee)
-{
-    while (begin != end) {
-        callee(*begin++);
-    }
-}
-
-template <class RandomAccessIter, class Callable>
-void CallStuff(RandomAccessIter begin, RandomAccessIter end, Callable callee, int threads) {
-    std::thread* roflmao = new std::thread[threads];
-    if (threads == 2) {
-        RandomAccessIter mid = begin + (end - begin) / 2;
-        roflmao[0] = std::thread([](RandomAccessIter begin, RandomAccessIter end, Callable callee) { CallStuff(begin, end, callee); }, begin, mid, callee);        
-        roflmao[1] = std::thread([](RandomAccessIter begin, RandomAccessIter end, Callable callee) { CallStuff(begin, end, callee); }, mid, end, callee);        
-    } else {
-//    	for (int i = 0; i < threads; ++i) {
-//           roflmao[i] = std::thread(CallStuff<RandomAccessIter, Callable>, (begin * (threads - i) + end * i) / threads, (begin * (threads - i - 1) + end * (i + 1)) / threads, callee);        
-//	}
-    }
-    for (int i = 0; i < threads; ++i) {
-        roflmao[i].join();
-    }
-    delete[] roflmao;
-}
+struct TOutputStream {
+}; 
 
 } // namespace olymplib
--- act_ut.cpp
+++ act_ut.cpp
@@ -3,65 +3,6 @@
 
 using namespace olymplib;
 
-class PreAllocatedActCorrectness : public ::testing::Test {
-protected:
-    static constexpr int TESTLEN = 1e8;
-    long long* memory;
-
-    PreAllocatedActCorrectness() {
-        memory = new long long[TESTLEN];
-        for (int i = 0; i < TESTLEN; ++i) {
-            memory[i] = i;
-        }
-    }
-
-    virtual ~PreAllocatedActCorrectness() {
-        for (int i = 0; i < TESTLEN; ++i) {
-            EXPECT_EQ(memory[i], 1LL * i * i);
-        }
-        delete[] memory;
-    }
-
-};
-
-TEST_F(PreAllocatedActCorrectness, SquareCorrectness) {
-    CallStuff(memory, memory + TESTLEN, [](long long& a) { a *= a; });
-}
-
-TEST_F(PreAllocatedActCorrectness, ParallelSquareCorrectness) {
-    CallStuff(memory, memory + TESTLEN, [](long long& a) { a *= a; }, 2);
-}
-
-class PreAllocatedActPerfomance : public ::testing::Test {
-protected:
-    static long long* memory;
-    static constexpr int TESTLEN = 1e9;
-
-    PreAllocatedActPerfomance() {
-    }
-
-    virtual ~PreAllocatedActPerfomance() {
-    }
-
-    static void SetUpTestCase() {
-      memory = new long long[TESTLEN];
-    }
-
-    static void TearDownTestCase() {
-      delete[] memory;
-    }
-};
-
-long long* PreAllocatedActPerfomance::memory;
-
-TEST_F(PreAllocatedActPerfomance, SquareCorrectness) {
-    CallStuff(memory, memory + TESTLEN, [](long long& a) { a *= a; });
-}
-
-TEST_F(PreAllocatedActPerfomance, ParallelSquareCorrectness) {
-    CallStuff(memory, memory + TESTLEN, [](long long& a) { a *= a; }, 2);
-}
-
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
